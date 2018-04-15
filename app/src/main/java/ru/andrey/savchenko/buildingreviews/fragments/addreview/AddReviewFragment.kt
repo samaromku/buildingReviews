7f3c98c7af5d78240b2095308cde0f730d091c01
savchenko <savchenko@magnit.ru>
2018-04-15 15:00:49 +0300

    add rating and add review fragment

--- app/src/main/java/ru/andrey/savchenko/buildingreviews/fragments/addreview/AddReviewFragment.kt
+++ app/src/main/java/ru/andrey/savchenko/buildingreviews/fragments/addreview/AddReviewFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_add_review.*
 import ru.andrey.savchenko.buildingreviews.R
 import ru.andrey.savchenko.buildingreviews.base.BaseFragment
 
@@ -13,5 +14,10 @@ import ru.andrey.savchenko.buildingreviews.base.BaseFragment
 class AddReviewFragment:BaseFragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_add_review, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rbRating.setOnRatingBarChangeListener { ratingBar, fl, b -> tvRating.text = "$fl из 5" }
     }
 }
