fc4f960aec0f47cf9df469e816279a0f041a8281
savchenko <savchenko@magnit.ru>
2018-04-28 12:48:38 +0300

    add auth button in error fragment

--- app/src/main/java/ru/andrey/savchenko/buildingreviews/fragments/ErrorFragment.kt
+++ app/src/main/java/ru/andrey/savchenko/buildingreviews/fragments/ErrorFragment.kt
@@ -1,18 +1,20 @@
 package ru.andrey.savchenko.buildingreviews.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.progress_error.*
 import ru.andrey.savchenko.buildingreviews.R
+import ru.andrey.savchenko.buildingreviews.activities.auth.AuthActivity
 import ru.andrey.savchenko.buildingreviews.base.BaseFragment
 
 /**
  * Created by savchenko on 28.04.18.
  */
-class ErrorFragment:BaseFragment() {
-    lateinit var error:String
+class ErrorFragment : BaseFragment() {
+    lateinit var error: String
     lateinit var repeat: () -> Unit
 
 
@@ -26,6 +28,11 @@ class ErrorFragment:BaseFragment() {
         btnRepeat.setOnClickListener {
             llProgressError.visibility = View.GONE
             repeat()
+        }
+        btnAuth.setOnClickListener { startActivity(Intent(activity, AuthActivity::class.java)) }
+        //auth failed
+        if (error.contains("403")) {
+            btnAuth.visibility = View.VISIBLE
         }
     }
 
