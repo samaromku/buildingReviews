d8dd4a532168c9a7e73a0f4d643d5d63dc80635d
Andrey <samaromku@mail.ru>
2018-05-20 11:21:47 +0300

    add base methods in linked list, now they dont work

--- app/src/test/java/ru/andrey/savchenko/buildingreviews/LinkedListTest.java
+++ app/src/test/java/ru/andrey/savchenko/buildingreviews/LinkedListTest.java
@@ -10,16 +10,78 @@ import java.util.LinkedList;
 
 public class LinkedListTest {
     @Test
-    public void testLinkedList(){
-        LinkedList<Integer>linkedList = new LinkedList<>();
+    public void testLinkedList() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
         linkedList.add(1);
         linkedList.add(39);
         linkedList.add(-1);
         linkedList.add(12);
         linkedList.add(120);
-        for(Integer i:linkedList){
-            System.out.println(i);
+//        for(Integer i:linkedList){
+//            System.out.println(i);
+//        }
+
+        MyLinkedList myLinkedList = new MyLinkedList<Integer>();
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        for (int i = 0; i < myLinkedList.size(); i++) {
+            System.out.println(myLinkedList.get(i));
+        }
+    }
+
+    class MyLinkedList<T> {
+        private Node<T> first;
+        private Node<T> last;
+        private int size;
+
+        //если это превый элемент, то заполняем first,
+        // если же нет, заполняем last, создаем цепочку промежуточных элементов, от first до last
+        public void add(T element) {
+            if (size == 0) {
+                first = new Node<T>(element);
+                last = new Node<T>(element);
+            } else if (size == 1) {
+                last = new Node<T>(element, first, null);
+                first.next = last;
+            } else {
+                Node<T> current = new Node<T>(element, first, last);
+                first.next = current;
+                last.previous = current;
+            }
+            size++;
         }
 
+        public T get(int index) {
+            Node<T> current = first;
+            for (int i = 0; i < index; i++) {
+                if (i == 0) {
+                    current = first;
+                } else {
+                    current = current.next;
+                }
+            }
+            return current.element;
+        }
+
+        public int size() {
+            return size;
+        }
+
+        private class Node<E> {
+            private E element;
+            private Node<E> previous;
+            private Node<E> next;
+
+            public Node(E element, Node<E> previous, Node<E> next) {
+                this.element = element;
+                this.previous = previous;
+                this.next = next;
+            }
+
+            public Node(E element) {
+                this.element = element;
+            }
+        }
     }
 }
